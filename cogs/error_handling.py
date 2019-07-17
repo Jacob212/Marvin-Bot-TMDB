fe8fa5f069f53f22efbba09984fbba901dfca1e5
Jacob Hale <jacobhale.hale@gmail.com>
2019-07-17 19:15:48 +0100

    changed ctx to context

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -6,23 +6,23 @@ class errorHandlingCog(commands.Cog):
     self.client = client
 
   @commands.Cog.listener()
-  async def on_command_error(self,ctx,error):
+  async def on_command_error(self,context,error):
     if isinstance(error, commands.NoPrivateMessage):
-      await ctx.message.channel.send("**private messages.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**private messages.**" + context.message.author.mention,delete_after=10)
     if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.message.channel.send("**Missing an argument.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**Missing an argument.**" + context.message.author.mention,delete_after=10)
     elif isinstance(error, commands.DisabledCommand):
-      await ctx.message.channel.send("**Command is disabled.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**Command is disabled.**" + context.message.author.mention,delete_after=10)
     elif isinstance(error, commands.CheckFailure):
-      await ctx.message.channel.send("**No permission.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**No permission.**" + context.message.author.mention,delete_after=10)
     elif isinstance(error, commands.CommandNotFound):
-      await ctx.message.channel.send("**Wrong command.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**Wrong command.**" + context.message.author.mention,delete_after=10)
     elif str(error) == "HTTPException: BAD REQUEST (status code: 400)":
-      await ctx.message.channel.send("**Too many characters.**" + ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("**Too many characters.**" + context.message.author.mention,delete_after=10)
     else:
-      embed = discord.Embed(title=str(error),description=f'{ctx.message.author.mention}\n{ctx.message.content}')
+      embed = discord.Embed(title=str(error),description=f'{context.message.author.mention}\n{context.message.content}')
       await self.client.get_channel(538719054479884300).send(embed=embed)
-      await ctx.message.channel.send("You either dont have access to the command or you have entered something wrong."+ctx.message.author.mention,delete_after=10)
+      await context.message.channel.send("You either dont have access to the command or you have entered something wrong."+context.message.author.mention,delete_after=10)
 
 
 
