3b624e29d992058a442329dea446cb7817d4b306
Jacob Hale <jacobhale.hale@gmail.com>
2019-07-31 01:10:58 +0100

    changed testing format

--- tests/test.py
+++ tests/test.py
@@ -15,8 +15,15 @@ if __name__ == "__main__":
 #creates instance of the bot
 client = commands.Bot(command_prefix="!", description="Testing Bot")
 
-async def test_search_blank():
-    await client.get_channel(603641157120950282).send("?search")
+basic_tests_list = (
+    ("?search", ("You have to enter something to search for.", "eg ?search the flash")),
+    ("?search the flash", ("Search results for:", "the flash")),
+    ("?search the flash -movies", ("Search results for:", "the flash")),
+    ("?search the flash -shows", ("Search results for:", "the flash"))
+    )
+
+async def basic_tests(test):
+    await client.get_channel(603641157120950282).send(test[0])
     print("Testing ?search")
     try:
         response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=10)
@@ -24,48 +31,16 @@ async def test_search_blank():
         print("failed")
         return "failed" 
     else:
-        if len(response.embeds) > 1 or response.embeds[0].title != "You have to enter something to search for." or response.embeds[0].description != "eg ?search the flash":
+        if len(response.embeds) > 1 or response.embeds[0].title != test[1][0] or response.embeds[0].description != test[1][1]:
             print("failed")
             return "failed"
         print("passed")
         return "passed"
 
-async def test_search():
-    await client.get_channel(603641157120950282).send("?search the flash")
-    print("Testing ?search the flash")
-    response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=10)
-    if len(response.embeds) > 1 or response.embeds[0].title != "Search results for:" or response.embeds[0].description != "the flash":
-        print("failed")
-        return "failed"
-    print("passed")
-    return "passed"
-
-async def test_search_movies():
-    await client.get_channel(603641157120950282).send("?search the flash -movies")
-    print("Testing ?search the flash -movies")
-    response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=10)
-    if len(response.embeds) > 1 or response.embeds[0].title != "Search results for:" or response.embeds[0].description != "the flash":
-        print("failed")
-        return "failed"
-    print("passed")
-    return "passed"
-
-async def test_search_shows():
-    await client.get_channel(603641157120950282).send("?search the flash -shows")
-    print("Testing ?search the flash -shows")
-    response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=10)
-    if len(response.embeds) > 1 or response.embeds[0].title != "Search results for:" or response.embeds[0].description != "the flash":
-        print("failed")
-        return "failed"
-    print("passed")
-    return "passed"
-
-tests_to_run = [test_search_blank,test_search,test_search_movies,test_search_shows]
-
 async def run_tests(tests):
     try:
         for test in tests:
-            result = await test()
+            result = await basic_tests(test)
             if result == "failed":
                 await client.get_channel(603641157120950282).send("?shutdown")
                 await client.close()
@@ -97,7 +72,7 @@ async def on_ready():#Prints that bot has logged in when ready.
     print('------')
     await asyncio.sleep(4)
     if str(client.get_guild(538718965904703508).get_member(530876853104410624).status) == "online":
-        await run_tests(tests_to_run)
+        await run_tests(basic_tests_list)
     else:
         await client.close()
         sys.exit(1)
