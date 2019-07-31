e33c53dfc077e48ced86e1399c2ee69dc0739f33
Jacob Hale <jacobhale.hale@gmail.com>
2019-07-31 17:55:32 +0100

    update to setup command and small other changes

--- cogs/management.py
+++ cogs/management.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from utils import api_handler
-from utils.sql import get_account_details, setup_account, update_access_token, update_list_id
+from utils.sql import get_account_details, setup_account, update_access_token, update_list_id, update_account
 
 AUTH = api_handler.Auth()
 LISTS = api_handler.Lists()
@@ -14,22 +14,25 @@ class ManagementCommands(commands.Cog):
     async def setup(self, context):
         await context.message.delete()
         account_details = get_account_details(context.author.id)
-        if account_details[0] is None:
+        if account_details is None or account_details[0] is None:
             await context.send("I have sent you more information to your DMs")
             response = AUTH.request()
             embed = discord.Embed(title="Link", description='To use all the features of this bot you will need an account with The Movie Database and approve the bot to have access to your account. Respond with "approved" when you have finshed the links instructions', url=f'https://www.themoviedb.org/auth/access?request_token={response.request_token}')
             await context.author.send(embed=embed)
             await self.client.wait_for('message', check=lambda m: isinstance(m.channel, discord.DMChannel) and m.content in ["approved", "Approved"])
             approved = AUTH.access(response.request_token)
-            if account_details[2] is None:
+            if account_details is None:
                 created_list = LISTS.create(approved.access_token)
                 setup_account(context.author.name, context.author.id, approved.access_token, approved.account_id, created_list.id)
+            elif account_details[2] is None:
+                created_list = LISTS.create(approved.access_token)
+                update_account(context.author.id, approved.access_token, approved.account_id, created_list.id)
             else:
                 update_access_token(context.author.id, approved.access_token)
         else:
             await context.send("You are already setup", delete_after=10)
 
-    @commands.command(description="", brief="!!Not working on the apis end.!!", aliases=["Unlink"], enabled=False)
+    @commands.command(description="", brief="!!Not working on the api's end.!!", aliases=["Unlink"], enabled=False)
     async def unlink(self, context):
         await context.message.delete()
         bots_message = await context.send("Are you sure you want to unlink your TMDb account from this bot.")
@@ -45,14 +48,15 @@ class ManagementCommands(commands.Cog):
     @commands.command(description="", brief="Used to create new list if old one was deleted.", aliases=["List"], enabled=False)#hard to test this right now
     async def list(self, context):
         await context.message.delete()
-        list_id = get_account_details(context.author.id)[2]
-        try:
-            LISTS.get(list_id)
-            await context.send("You already have a list set up.", delete_after=10)
-        except Exception as e:
-            raise e
+        account_details = get_account_details(context.author.id)
+        if account_details[2] is None:
+            response = LISTS.create(account_details[1])
+            print(response.status_message)
+            await context.send("A new list has been created.", delete_after=10)
+        else:
+            await context.send("There is already a list saved under your account.", delete_after=10)
 
-    @commands.command(description="", brief="!!Not working on the apis end.!! Deletes your watched list. This cannot be undone.", aliases=["Delete"], enabled=False)
+    @commands.command(description="", brief="!!Not working on the api's end.!! Deletes your watched list. This cannot be undone.", aliases=["Delete"], enabled=False)
     async def delete(self, context):
         await context.message.delete()
         bots_message = await context.send("Are you sure you want to delete your watched list.")
@@ -61,7 +65,7 @@ class ManagementCommands(commands.Cog):
         reaction, user = await self.client.wait_for("reaction_add", check=lambda r, u: r.emoji in ["✅", "❌"] and u.id == context.message.author.id and r.message.id == bots_message.id)
         if reaction.emoji == "✅":
             account_details = get_account_details(context.author.id)
-            LISTS.delete(account_details[2], account_details[0])
+            result = LISTS.delete(account_details[2], account_details[0])
             update_list_id(context.author.id, None)
         await bots_message.delete()
 
