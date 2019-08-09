cb4bb58ed10ef54f279ee4ce9858f58f40bf300a
Jacob Hale <jacobhale.hale@gmail.com>
2019-08-09 23:07:24 +0100

    added test for changing pages and getting details

--- tests/test.py
+++ tests/test.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
 #creates instance of the bot
 client = commands.Bot(command_prefix="!", description="Testing Bot")
 
-basic_tests_list = (
+basic_embed_tests = (
     ("?search", ("You have to enter something to search for.", "eg ?search the flash")),
     ("?search the flash", ("Search results for:", "the flash")),
     ("?search the flash -movies", ("Search results for:", "the flash")),
     ("?search the flash -shows", ("Search results for:", "the flash")),
-    ("?search this isnt a in here", ("Search results for:", "this isnt a in here"))
+    ("?search this isnt in here", ("Search results for:", "this isnt in here"))
     )
 
 async def basic_tests(test):
@@ -29,39 +29,65 @@ async def basic_tests(test):
         response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=20)
     except asyncio.futures.TimeoutError:
         print(f'{test[0]} timeout')
-        return "failed" 
-    else:
-        if len(response.embeds) > 1 or response.embeds[0].title != test[1][0] or response.embeds[0].description != test[1][1]:
-            print(f'{test[0]} failed')
-            return "failed"
-        print(f'{test[0]} passed')
-        return "passed"
+        return "failed"
+    if len(response.embeds) > 1 or response.embeds[0].title != test[1][0] or response.embeds[0].description != test[1][1]:
+        print(f'{test[0]} failed')
+        return "failed"
+    print(f'{test[0]} passed')
+    return "passed"
 
-async def run_tests(tests):
+async def reactions_test():
+    await client.get_channel(603641157120950282).send("?search the flash")
     try:
-        for test in tests:
-            result = await basic_tests(test)
-            if result == "failed":
-                await client.get_channel(603641157120950282).send("?shutdown")
-                await client.close()
-                sys.exit(1)
+        response = await client.wait_for("message", check=lambda m: m.author.id == 530876853104410624, timeout=20)
+    except asyncio.futures.TimeoutError:
+        print(f'reactions test timeout')
+        return "failed"
+    await asyncio.sleep(1)
+    await response.channel.send("1")
+    await asyncio.sleep(1)
+    message = await response.channel.fetch_message(response.id)
+    if response.embeds[0].title != "The Flash":
+        print("failed at getting details")
+        return "failed"
+    await asyncio.sleep(1)
+    await response.add_reaction("◀")
+    await asyncio.sleep(1)
+    message2 = await response.channel.fetch_message(response.id)
+    if message.embeds[0] == message2.embeds[0]:
+        print("failed when going back")
+        return "failed"
+    await asyncio.sleep(1)
+    await response.add_reaction("▶")
+    await asyncio.sleep(1)
+    message = await response.channel.fetch_message(response.id)
+    if response.embeds[0] == message.embeds[0]:
+        print("failed to get to page 2")
+        return "failed"
+    print("reactions test passed")
+    return "passed"
+
+async def run_tests():
+    # for test in basic_embed_tests:
+    #     result = await basic_tests(test)
+    #     if result == "failed":
+    #         await asyncio.sleep(2)
+    #         await client.get_channel(603641157120950282).send("?shutdown")
+    #         await client.close()
+    #         sys.exit(1)
+    #     await asyncio.sleep(2)
+    # else:
+        result = await reactions_test()
+        if result == "failed":
             await asyncio.sleep(2)
-    except Exception as e:
-        raise e
-        await asyncio.sleep(2)
-        await client.get_channel(603641157120950282).send("?shutdown")
-        await client.close()
-        sys.exit(1)
-    else:
+            await client.get_channel(603641157120950282).send("?shutdown")
+            await client.close()
+            sys.exit(1)
         print("passed all tests")
         await asyncio.sleep(2)
         await client.get_channel(603641157120950282).send("?shutdown")
         await client.close()
         sys.exit(0)
-
-@client.command()
-async def stop(context):
-    await client.close()
 
 @client.event
 async def on_ready():#Prints that bot has logged in when ready.
@@ -72,7 +98,7 @@ async def on_ready():#Prints that bot has logged in when ready.
     print('------')
     await asyncio.sleep(4)
     if str(client.get_guild(538718965904703508).get_member(530876853104410624).status) == "online":
-        await run_tests(basic_tests_list)
+        await run_tests()
     else:
         await client.close()
         sys.exit(1)
